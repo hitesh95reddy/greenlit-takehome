f25aefa556d35322b033c12f3e6229e1437de247
hitesh95reddy <33710615+hitesh95reddy@users.noreply.github.com>
2024-02-23 20:12:16 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DB_URL="postgresql://postgres:postgres@localhost:5432/greenlit"
+engine=create_engine(DB_URL)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base=declarative_base()
