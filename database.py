6ce0694f7c3449aa548881e20bd5d67b7298deda
hitesh95reddy <33710615+hitesh95reddy@users.noreply.github.com>
2024-02-23 21:18:04 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL="postgresql://postgres:postgres@localhost:5432/greenlit"
+#DB_URL="postgresql://postgres:postgres@localhost:5432/greenlit"
+DB_URL='postgresql://ujwxfyrh:IHsQGSgsAEYjeFLfgyVJ_08kInRWRU17@abul.db.elephantsql.com/ujwxfyrh'
 engine=create_engine(DB_URL)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
